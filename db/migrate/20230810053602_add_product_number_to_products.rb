5f88f787944fd5870feca66676a12f85c749f74f
hariom <hariompatidar32498@gmail.com>
2023-08-10 12:18:53 +0530

    create migrate

---
+++ db/migrate/20230810053602_add_product_number_to_products.rb
@@ -0,0 +1,4 @@
+class AddProductNumberToProducts < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_113206) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "occupation"
+
+    t.timestamps
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+
   end
 
 end
